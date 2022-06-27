e2e600325dc6b12aad93cf8642a1dba3700611d6
Rohit Patidar <39286352+patidarrohit@users.noreply.github.com>
2022-06-27 14:29:34 -0400

    Initial Commit

---
+++ war-challange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-challange
+//
+//  Created by Rohit Patidar on 6/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challange/war_challangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challangeApp.swift
+//  war-challange
+//
+//  Created by Rohit Patidar on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
