5434edf9699f8e92af44b81f6b901db433854ebe
Rohit Patidar <39286352+patidarrohit@users.noreply.github.com>
2022-06-27 17:40:04 -0400

    First Commit

--- war-challange/ContentView.swift
+++ war-challange/ContentView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card2"
+    @State var playerScore = 0
+    @State var cpuCard = "card3"
+    @State var cpuScore = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background").resizable(resizingMode: .stretch).ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                    } else if playerRand < cpuRand{
+                        cpuScore += 1
+                    }
+                    
+                } label: {
+                    Image("dealbutton")
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+
+            }
+        }
     }
 }
 
